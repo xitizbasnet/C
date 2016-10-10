960acbf63148aca052707139fb62e3834346dbbc
xitiz basnet <xitiz.basnet1@gmail.com>
2016-10-10 07:55:10 +0545

    sum of three number

---
+++ sum of three numbers.C
@@ -0,0 +1,31 @@
+/*
+Student Name:xitiz basnet
+Subject:Programming Fundamentals
+Roll No.:
+Program:Program to Print Sum of Three Numbers.
+Lab No.:02
+Date:19 sep,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+	{
+	int a,b,c,sum;
+
+	
+	printf("Enter First Number:\n");
+	scanf("%d",&a);
+	
+	printf("Enter Second Number:\n");
+	scanf("%d",&b);
+	
+	printf("Enter Third Number:\n");
+	scanf("%d",&c);
+	
+	sum=a+b+c;
+	printf("Sum of Three Numbers %d,%d,%d=%d",a,b,c,sum);
+	
+	getch();
+     }
