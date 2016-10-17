aff1aee2cfd5ad3cdb329c431e4b578cbc9c26d8
xitiz basnet <xitiz.basnet1@gmail.com>
2016-10-17 10:32:04 +0545

    enter your name

---
+++ printxitiz.c
@@ -0,0 +1,19 @@
+/* 
+  student name:xitiz basnet
+  subject: programming fundamentals
+  roll no:
+  program:print your name
+  lab no:
+  date:oct17,2016
+ */
+ 
+ #include<stdio.h>  
+ 
+ 
+ int main(){
+	  
+	  printf("my name is xitiz basnet");
+    return (0);
+  
+  }
+ 
