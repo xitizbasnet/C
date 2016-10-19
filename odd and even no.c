8c3a2418b378f1b16007484373ecb3beb8fe4a71
xitiz basnet <xitiz.basnet1@gmail.com>
2016-10-19 12:24:13 +0545

    find the given no is odd or even
    find the given no is odd or even

---
+++ odd and even no.c
@@ -0,0 +1,28 @@
+/*
+ student name: xitiz basnet
+ subject: fundamentals of programming languages
+ roll no:36
+ BCS semister: 1st 
+ program: write a program to find the given no is odd or even 
+ lab no:03
+ date: 19oct,2016
+ */
+ 
+ #include<stdio.h>
+ //#include<conio.h>
+ 
+ int main(){
+	 
+	 int z;
+	 
+	printf("Enter any number:\n");
+	 scanf("%d",&z);
+	 
+	  if(z%2==0)
+	 printf("Your number is even");
+	 
+	 else
+	 printf("Your number is odd");
+	 
+	 return 0;
+	 }
