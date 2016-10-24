dab896b8472cdc0524188e1e9ae56c21c2a20994
xitiz basnet <xitiz.basnet1@gmail.com>
2016-10-24 10:40:42 +0545

    largest and smallest number using terniary value
    largest and smallest number using terniary value

---
+++ terminiary operator.c
@@ -0,0 +1,26 @@
+/*
+student name: xitiz basnet
+subject:fundamentals of programminf languages
+roll no:36
+program: write a c program to take two i/p from user and print largest and smallest nos. using terinary operator.
+lab no: 07
+date:24 oct, 2016
+*/
+
+#include<stdio.h>
+//#include<conio.h>
+
+int main()
+{
+int a,b;
+                 printf("Enter the first number(a):\n");
+                 scanf("%d",&a);
+
+                 printf("Enter the second number(b):\n");
+                 scanf("%d",&b);
+
+(a>b)? printf("The largest number is:a") : printf("The largest number is:b");
+
+return 0;
+}
+                
