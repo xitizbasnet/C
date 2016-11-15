08d01fad492cc5a40b59afd1b08f2374a6a042d6
xitiz basnet <xitiz.basnet1@gmail.com>
2016-11-15 10:11:23 +0545

    Print your name 10 times.
    Print your name 10 times.

---
+++ print_name_10_times.c
@@ -0,0 +1,18 @@
+/*
+Student name:xitiz basnet
+roll no:28
+Program:write a c program to print your name 10 times.
+Lab no: 14
+Date:11/15/2016
+BCS: 1st Sem
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i;
+    for(i=0;i<10;i++)
+    printf("xitiz basnet\n");
+    return 0;
+    getch();
+}
