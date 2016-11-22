ef0192e3c8c35cf79f6e8defd9b7dbb16db861e4
xitiz basnet <xitiz.basnet1@gmail.com>
2016-11-22 10:44:02 +0545

    first n odd terms
    first n odd terms

---
+++ first-n_odd-terms.c
@@ -0,0 +1,31 @@
+/*
+student name: xitiz basnet
+subject:fundamentals of programminf languages
+roll no:36
+program: write a c program to generate first n odd terms and find their sum also,
+lab no: 16
+date:22 nov, 2016
+*/
+
+#include<stdio.h>
+//#include<conio.h>
+
+int main()
+{
+int a,i,sum=0;
+       printf("Enter the number(a):\n");
+       scanf("%d",&a);
+   
+for(i=1;i<=a;i++) 
+{     
+    if(i%2!=0)
+    {
+    
+    sum+=i;
+    printf("sum=%d\n",sum);
+    }
+    
+}
+printf("sum of the odd number=%d",sum);
+return 0;
+}
