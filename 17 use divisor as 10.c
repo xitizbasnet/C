299c64891cdc61a98c233044ea3a5ca9b3bbd4ce
xitiz basnet <xitiz.basnet1@gmail.com>
2016-11-24 10:00:21 +0545

    use as divisor
    use as divisor

---
+++ 17 use divisor as 10.c
@@ -0,0 +1,20 @@
+/*
+student name:xitiz basnet
+subject:programming fundamental
+lab:17
+semister:1st
+program:use divisor as 10
+date:11/24/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,x;
+printf("Enter a number:");
+scanf("%d",&a);
+x=(a/10);
+printf("Remainder=%d",x);
+getch();
+return 0;
+}
